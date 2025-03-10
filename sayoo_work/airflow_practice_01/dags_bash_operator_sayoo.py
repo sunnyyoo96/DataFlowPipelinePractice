d2aa0e6fa3a71a7a84e8736ad6bcea79e0e46079
sayu <sayu@zalesia.com>
2025-03-10 21:37:18 +0900

    2025-03-11 - 수정

--- sayoo_work/airflow_practice_01/dags_bash_operator_sayoo.py
+++ sayoo_work/airflow_practice_01/dags_bash_operator_sayoo.py
@@ -17,7 +17,6 @@ with DAG(
     schedule_interval='0 0 * * *',
     start_date=pendulum.datetime(2025, 3, 11, tz="Asia/Seoul"),
     catchup=False,
-    dagrun_timeout=timedelta(minutes=60),
     tags=['sa'],
 ) as dag:
     bash_t1 = BashOperator(
