8863745ff13961719fe207c16ae1773ca2aa8ce2
sayu <sayu@zalesia.com>
2025-03-10 22:03:52 +0900

    2025-03-10 - 날짜변경

--- sayoo_work/airflow_practice_01/dags_bash_operator_sayoo.py
+++ sayoo_work/airflow_practice_01/dags_bash_operator_sayoo.py
@@ -15,7 +15,7 @@ import pendulum
 with DAG(
     dag_id='dags_bash_operator_sayoo',
     schedule_interval='0 0 * * *',
-    start_date=pendulum.datetime(2025, 3, 11, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2025, 3, 10, tz="Asia/Seoul"),
     catchup=False,
     tags=['sa']
 ) as dag:
