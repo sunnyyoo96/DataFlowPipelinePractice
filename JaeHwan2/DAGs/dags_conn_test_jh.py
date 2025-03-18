d9684d26c50aa813eb9797b3de5d62e861d8d237
DESKTOP-SBO469G\JaeHwan <cjh8523@naver.com>
2025-03-18 20:34:37 +0900

    2025-03-18-JH-CONN-TEST

---
+++ JaeHwan2/DAGs/dags_conn_test_jh.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from datetime import datetime, timedelta
+import pendulum
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+# In[ ]:
+
+
+with DAG(
+    dag_id='dags_conn_jh',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2025, 3, 18,tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    t1 = EmptyOperator(
+        task_id="t1"
+    )
+
+    t2 = EmptyOperator(
+        task_id="t2"
+    )
+
+    t3 = EmptyOperator(
+        task_id="t3"
+    )
+
+    t4 = EmptyOperator(
+        task_id="t4"
+    )
+
+    t5 = EmptyOperator(
+        task_id="t5"
+    )
+
+    t6 = EmptyOperator(
+        task_id="t6"
+    )
+
+    t7 = EmptyOperator(
+        task_id="t7"
+    )
+
+    t8 = EmptyOperator(
+        task_id="t8"
+    )
+
+    t1 >> [t2,t3] >> t4
+    t5 >> t4
+    [t4,t7] >> t6 >> t8
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
