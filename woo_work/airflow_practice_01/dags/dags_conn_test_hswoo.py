d1c8020c4083b9ac91d1af31d07dd205bbde075d
woohs0930 <woohs0930@gmail.com>
2025-03-18 20:34:10 +0900

    2025-03-18 hswoo hswoo 명칭 변경

--- woo_work/airflow_practice_01/dags/dags_conn_test_hswoo.py
+++ woo_work/airflow_practice_01/dags/dags_conn_test_hswoo.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.operators.empty import EmptyOperator  # EmptyOperator -> 값이 없는 작업
 
 with DAG(
-        dag_id="dags_conn_test",
+        dag_id="dags_conn_test_hswoo",
         schedule=None,
         start_date=pendulum.datetime(2025, 3, 18, tz="Asia/Seoul"),
         catchup=False,
