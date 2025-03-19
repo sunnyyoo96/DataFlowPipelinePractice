649ade1d95a54a4c38d05f1e745e38660a6e1d7a
DESKTOP-SBO469G\JaeHwan <cjh8523@naver.com>
2025-03-19 19:48:32 +0900

    email_푸시

---
+++ JaeHwan2/DAGs/dags_email_operator_jh.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from datetime import datetime, timedelta
+import pendulum
+
+from airflow import DAG
+from airflow.operators.email import EmailOperator
+
+
+# In[ ]:
+
+
+with DAG(
+    dag_id='dags_email_operator_jh',
+    schedule_interval="0 20 1 * *",
+    start_date=pendulum.datetime(2025, 3, 19,tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    send_email_task = EmailOperatoe(
+        task_id = "send_email_task",
+        to = "cjh35201@gmail.com",
+        subject = "Airflow 성공메일"
+        html_content = "Airflow 작업이 완료되었습니다."
+    )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
