4b348c7afe057df6d5c9258f23dfc51ded1636bc
sunnyyoo96 <y4380@naver.com>
2025-03-21 20:30:27 +0900

    2025-03-21 - python-decorator @task

---
+++ sayoo_work/airflow_practice_01/dags_python_task_decorator_sayoo.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import logging
+import sys
+import time
+from pprint import pprint
+
+import pendulum
+from airflow import DAG
+from airflow.decorators import dag, task
+
+
+with DAG(
+    dag_id="example_python_task_decorator_sayoo",
+    schedule= "30 20 * * 5#3",  ## 셋째주 금요일 8시 30분 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 3, 21, tz="UTC"),
+    catchup=False,
+    tags=["sayoo"],
+):
+    # @task(task_id="python_task_1_sayoo")
+    # def print_context(ds=None, **kwargs):  ## default 인자값 None , **keyword argument
+    #     """
+    #     *kwargs: 일반적인 인자 나열 방식으로 호출합니다. 예: func(1, 2, 3)
+    #     **kwargs: 키-값 쌍 형태로 호출합니다. 예: func(a=1, b=2, c=3)
+    #     Print the Airflow context and ds variable from the context."""
+    #     pprint(kwargs)
+    #     print(ds)
+    #     return "Whatever you return gets printed in the logs"
+
+
+    @task(task_id="python_task_1_sayoo")
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1_sayoo = print_context('task_decorator 실행')
