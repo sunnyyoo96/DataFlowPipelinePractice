cb5554e3f76bb65277c893eaa5b493f0195e962c
sunnyyoo96 <y4380@naver.com>
2025-03-25 20:20:14 +0900

    2025-03-25 dags_python_with_pi_args_sayoo

--- sayoo_work/airflow_practice_01/dags_python_task_decorator_sayoo.py
+++ sayoo_work/airflow_practice_01/dags_python_task_decorator_sayoo.py
@@ -20,12 +20,16 @@ with DAG(
     # @task(task_id="python_task_1_sayoo")
     # def print_context(ds=None, **kwargs):  ## default 인자값 None , **keyword argument
     #     """
-    #     *kwargs: 일반적인 인자 나열 방식으로 호출합니다. 예: func(1, 2, 3)
+    #     *args: 일반적인 인자 나열 방식으로 호출합니다. 예: func(1, 2, 3)
     #     **kwargs: 키-값 쌍 형태로 호출합니다. 예: func(a=1, b=2, c=3)
     #     Print the Airflow context and ds variable from the context."""
     #     pprint(kwargs)
     #     print(ds)
     #     return "Whatever you return gets printed in the logs"
+    #     인자 형식
+    #     *args : tuple index 통해서 접근 - args[0], args[1]
+    #     **kwargs : key-value 형태 Ex) name='hjkim' country='kr'
+    #     kwargs.get('name') or ''    없다면 None
 
 
     @task(task_id="python_task_1_sayoo")
