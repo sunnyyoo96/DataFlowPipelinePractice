0a9cefb775bdafce18951389967b883d9f3aeb0a
woohs0930 <woohs0930@gmail.com>
2025-03-31 20:49:16 +0900

    2025-03-31 hswoo task 순서 위치 조정

--- woo_work/airflow_practice_01/dags/dags_python_template_hswoo.py
+++ woo_work/airflow_practice_01/dags/dags_python_template_hswoo.py
@@ -10,16 +10,17 @@ with DAG(
         catchup=False,
         tags=['hswoo'],
 ) as dag:
-
     def python_function1(start_date, end_date, **kwargs):
         print(start_date)
         print(end_date)
+
 
     python_t1 = PythonOperator(
         task_id='python_t1',
         python_callable=python_function1,
         op_kwargs={'start_date': '{{data_interval_start | ds }}', 'end_date': '{{data_interval_end | ds }}'},
     )
+
 
     @task(task_id='python_t2')
     def python_function2(**kwargs):
@@ -30,4 +31,5 @@ with DAG(
         print('data_interval_end:' + kwargs['data_interval_end'])
         print('task_instance:' + kwargs['ti'])
 
-        python_t1 >> python_function2()
+
+    python_t1 >> python_function2()
