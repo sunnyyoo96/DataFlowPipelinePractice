2f8b259b91e8dcdcf2fa52a7c02dd43814a4150e
DESKTOP-ABQ39QB\minyoung <minyoung3578@gmail.com>
2025-03-31 19:50:25 +0900

    2025-03-31 템플릿

---
+++ minyoung/dags/dags_python_show_templates_my.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+
+# In[ ]:
+
+
+with DAG(
+    dag_id = "dags_python_show_templates_my",
+    shedule = "00 20 * * *",
+    start_date = pendulum.datetime(2025, 4, 7, tz = "Asia/Seoul"),
+    tags = ['mychoi'],
+    catchup = True
+) as dag :
+
+    @task(task_id = 'python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
+
