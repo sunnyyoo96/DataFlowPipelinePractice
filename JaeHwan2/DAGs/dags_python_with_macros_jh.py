b01260185a38df7ce56fce1dcf3cf0e15b57e383
DESKTOP-SBO469G\JaeHwan <cjh8523@naver.com>
2025-04-02 21:32:39 +0900

    no message

--- JaeHwan2/DAGs/dags_python_with_macros_jh.py
+++ JaeHwan2/DAGs/dags_python_with_macros_jh.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[5]:
 
 
 from datetime import datetime, timedelta
@@ -13,7 +13,7 @@ from dateutil import relativedelta
 from airflow.decorators import task
 
 
-# In[ ]:
+# In[7]:
 
 
 with DAG(
@@ -27,23 +27,23 @@ with DAG(
         templates_dict={'start_date':'{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
                          'end_date': '{{(data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
                      })
-     def get_datetime_macro(**kwargs):
-         templates_dict = kwargs.get('templates_dict') or {}
-         if templates_dict:
-             start_date = templates_dict.get('start_date') or 'start_date없음'
-             end_date = templates_dict.get('end_date') or 'end_date없음'
-             print(start_date)
-             print(end_date)
+    def get_datetime_macro(**kwargs):
+        templates_dict = kwargs.get('templates_dict') or {}
+        if templates_dict:
+            start_date = templates_dict.get('start_date') or 'start_date없음'
+            end_date = templates_dict.get('end_date') or 'end_date없음'
+            print(start_date)
+            print(end_date)
 
     @task(task_id='task_direct_calc')
      def get_datetime_calc(**kwargs):
-         from dateutil.relativedelta import relativedelta
+        from dateutil.relativedelta import relativedelta
          
-         data_interval_end = kwargs['data_interval_end']
-         prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1, day=1)
-         prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
-         print(prev_month_day_first.strftime('%Y-%m-%d'))
-         print(prev_month_day_last.strftime('%Y-%m-%d'))
+        data_interval_end = kwargs['data_interval_end']
+        prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1, day=1)
+        prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
+        print(prev_month_day_first.strftime('%Y-%m-%d'))
+        print(prev_month_day_last.strftime('%Y-%m-%d'))
          
     get_datetime_macro() >> get_datetime_calc()
     
